61fdbbb75a20decdc6ea4b1ba24d92060b7de515
PeyHunter <Nando.goat@gmail.com>
2025-02-19 21:29:32 +0100

    Update CarDAO.java

--- src/DAO/CarDAO.java
+++ src/DAO/CarDAO.java
@@ -1,5 +1,50 @@
 package DAO;
 
-public class CarDAO
+import Models.Car;
+import Models.Person;
+
+import java.sql.Connection;
+import java.util.List;
+
+
+public class CarDAO implements GenericDAO<Car>
 {
+    private Connection connection;
+
+    public CarDAO(Connection connection)
+    {
+        this.connection = connection;
+    }
+
+    @Override
+    public void insert(Car car)
+    {
+
+    }
+
+    @Override
+    public void update(Car car)
+    {
+
+    }
+
+
+    @Override
+    public void delete(Car car)
+    {
+
+    }
+
+    @Override
+    public void selectById(Car car)
+    {
+
+    }
+
+    public List<Car> selectAll()
+    {
+        return ;
+    }
+
+
 }
